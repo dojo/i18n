d8d63cf05232c86f045e4daba338ebde5ae1749f
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2015-07-17 09:52:37 -0700

    globalize.d.ts: fix module definition and export

--- typings/globalize/globalize.d.ts
+++ typings/globalize/globalize.d.ts
@@ -3,85 +3,87 @@
 declare module 'globalize' {
 	import Cldr = require('cldr');
 
-	export interface CurrencyFormatter {
-		(value: number): string;
+	module Globalize {
+		export interface CurrencyFormatter {
+			(value: number): string;
+		}
+
+		export interface CurrencyOptions extends NumberOptions {
+			style?: string;
+		}
+
+		export interface DateFormatter {
+			(value: Date): string;
+		}
+
+		export interface DateParser {
+			(value: string): Date;
+		}
+
+		export interface DateOptions {
+			date?: string;
+			datetime?: string;
+			raw?: string;
+			skeleton?: string;
+			time?: string;
+		}
+
+		export interface MessageFormatter {
+			(variables: string | string[] | Object): string;
+			(...variables: string[]): string;
+		}
+
+		export interface NumberFormatter {
+			(value: number): string;
+		}
+
+		export interface NumberOptions {
+			maximumFractionDigits?: number;
+			maximumSignificantDigits?: number;
+			minimumFractionDigits?: number;
+			minimumIntegerDigits?: number;
+			minimumSignificantDigits?: number;
+			round?: string;
+			style?: string;
+			useGrouping?: Boolean;
+		}
+
+		export interface NumberParser {
+			(value: string): number;
+		}
+
+		export interface NumberParserOptions {
+			style?: string;
+		}
+
+		export interface Pluralizer {
+			(value: number): string;
+		}
+
+		export interface PluralOptions {
+			type: string;
+		}
+
+		export interface TimeFormatter {
+			(value: number): string;
+		}
+
+		export interface TimeOptions {
+			form: string;
+		}
 	}
 
-	export interface CurrencyOptions {
-		style?: string;
-	}
-
-	export interface DateFormatter {
-		(value: Date): string;
-	}
-
-	export interface DateParser {
-		(value: string): Date;
-	}
-
-	export interface DateOptions {
-		date?: string;
-		datetime?: string;
-		raw?: string;
-		skeleton?: string;
-		time?: string;
-	}
-
-	export interface MessageFormatter {
-		(variables: string | string[] | Object): string;
-		(...variables: string[]): string;
-	}
-
-	export interface NumberFormatter {
-		(value: number): string;
-	}
-
-	export interface NumberOptions {
-		maximumFractionDigits?: number;
-		maximumSignificantDigits?: number;
-		minimumFractionDigits?: number;
-		minimumIntegerDigits?: number;
-		minimumSignificantDigits?: number;
-		round?: string;
-		style?: string;
-		useGrouping?: Boolean;
-	}
-
-	export interface NumberParser {
-		(value: string): number;
-	}
-
-	export interface NumberParserOptions {
-		style?: string;
-	}
-
-	export interface Pluralizer {
-		(value: number): string;
-	}
-
-	export interface PluralOptions {
-		type: string;
-	}
-
-	export interface TimeFormatter {
-		(value: number): string;
-	}
-
-	export interface TimeOptions {
-		form: string;
-	}
-
-	export default class Globalize {
+	class Globalize {
 		/**
 		 * @requires globalize/currency
 		 * @param currencyCode ISO 4217 (e.g. 'USD')
 		 */
-		static currencyFormatter(currencyCode: string, options?: CurrencyOptions): CurrencyFormatter;
+		static currencyFormatter(currencyCode: string, options?: Globalize.CurrencyOptions): Globalize.CurrencyFormatter;
 
 		/**
 		 * @requires globalize/date
 		 */
-		static dateFormatter(options: DateOptions): DateFormatter;
+		static dateFormatter(options: Globalize.DateOptions): Globalize.DateFormatter;
 
 		/**
 		 * @requires globalize/date
@@ -91,12 +93,12 @@ declare module 'globalize' {
 		/**
 		 * @requires globalize/currency
 		 */
-		static formatCurrency(value: number, currencyCode: string, options?: CurrencyOptions): string;
+		static formatCurrency(value: number, currencyCode: string, options?: Globalize.CurrencyOptions): string;
 
 		/**
 		 * @requires globalize/date
 		 */
-		static formatDate(value: Date, options?: DateOptions): string;
+		static formatDate(value: Date, options?: Globalize.DateOptions): string;
 
 		/**
 		 * @requires globalize/date
@@ -107,12 +109,12 @@ declare module 'globalize' {
 		/**
 		 * @requires globalize/number
 		 */
-		static formatNumber(value: number, options?: NumberOptions): string;
+		static formatNumber(value: number, options?: Globalize.NumberOptions): string;
 
 		/**
 		 * @requires globalize/relative-time
 		 */
-		static formatRelativeTime(value: number, unit: string, options?: TimeOptions): string;
+		static formatRelativeTime(value: number, unit: string, options?: Globalize.TimeOptions): string;
 
 		static load(...cldrData: Object[]): void;
 
@@ -126,27 +128,27 @@ declare module 'globalize' {
 		/**
 		 * @requires globalize/message
 		 */
-		static messageFormatter(path: string | string[]): MessageFormatter;
+		static messageFormatter(path: string | string[]): Globalize.MessageFormatter;
 
 		/**
 		 * @requires globalize/number
 		 */
-		static numberFormatter(options: NumberOptions): NumberFormatter;
+		static numberFormatter(options: Globalize.NumberOptions): Globalize.NumberFormatter;
 
 		/**
 		 * @requires globalize/number
 		 */
-		static numberParser(options: NumberParserOptions): NumberParser;
+		static numberParser(options: Globalize.NumberParserOptions): Globalize.NumberParser;
 
 		/**
 		 * @requires globalize/plural
 		 */
-		static pluralGenerator(options: PluralOptions): Pluralizer;
+		static pluralGenerator(options: Globalize.PluralOptions): Globalize.Pluralizer;
 
 		/**
 		 * @requires globalize/relative-time
 		 */
-		static relativeTimeFormatter(unit: string, options?: TimeOptions): TimeFormatter;
+		static relativeTimeFormatter(unit: string, options?: Globalize.TimeOptions): Globalize.TimeFormatter;
 
 		constructor(locale: string | Cldr);
 
@@ -154,27 +156,27 @@ declare module 'globalize' {
 		 * @requires globalize/currency
 		 * @param currencyCode ISO 4217 (e.g. 'USD')
 		 */
-		currencyFormatter(currencyCode: string, options?: CurrencyOptions): CurrencyFormatter;
+		currencyFormatter(currencyCode: string, options?: Globalize.CurrencyOptions): Globalize.CurrencyFormatter;
 
 		/**
 		 * @requires globalize/date
 		 */
-		dateFormatter(options: DateOptions): DateFormatter;
+		dateFormatter(options: Globalize.DateOptions): Globalize.DateFormatter;
 
 		/**
 		 * @requires globalize/date
 		 */
-		dateParser(options: DateOptions): DateParser;
+		dateParser(options: Globalize.DateOptions): Globalize.DateParser;
 
 		/**
 		 * @requires globalize/currency
 		 */
-		formatCurrency(value: number, currencyCode: string, options?: CurrencyOptions): string;
+		formatCurrency(value: number, currencyCode: string, options?: Globalize.CurrencyOptions): string;
 
 		/**
 		 * @requires globalize/date
 		 */
-		formatDate(value: Date, options?: DateOptions): string;
+		formatDate(value: Date, options?: Globalize.DateOptions): string;
 
 		/**
 		 * @requires globalize/date
@@ -185,51 +187,53 @@ declare module 'globalize' {
 		/**
 		 * @requires globalize/number
 		 */
-		formatNumber(value: number, options?: NumberOptions): string;
+		formatNumber(value: number, options?: Globalize.NumberOptions): string;
 
 		/**
 		 * @requires globalize/relative-time
 		 */
-		formatRelativeTime(value: number, unit: string, options?: TimeOptions): string;
+		formatRelativeTime(value: number, unit: string, options?: Globalize.TimeOptions): string;
 
 		/**
 		 * @requires globalize/message
 		 */
-		messageFormatter(path: string | string[]): MessageFormatter;
+		messageFormatter(path: string | string[]): Globalize.MessageFormatter;
 
 		/**
 		 * @requires globalize/number
 		 */
-		numberFormatter(options: NumberOptions): NumberFormatter;
+		numberFormatter(options: Globalize.NumberOptions): Globalize.NumberFormatter;
 
 		/**
 		 * @requires globalize/number
 		 */
-		numberParser(options: NumberParserOptions): NumberParser;
+		numberParser(options: Globalize.NumberParserOptions): Globalize.NumberParser;
 
 		/**
 		 * @requires globalize/date
 		 */
-		parseDate(value: string, options?: DateOptions): Date;
+		parseDate(value: string, options?: Globalize.DateOptions): Date;
 
 		/**
 		 * @requires globalize/number
 		 */
-		parseNumber(value: string, options?: NumberParserOptions): number;
+		parseNumber(value: string, options?: Globalize.NumberParserOptions): number;
 
 		/**
 		 * @requires globalize/plural
 		 */
-		plural(value: number, options?: PluralOptions): string;
+		plural(value: number, options?: Globalize.PluralOptions): string;
 
 		/**
 		 * @requires globalize/plural
 		 */
-		pluralGenerator(options: PluralOptions): Pluralizer;
+		pluralGenerator(options: Globalize.PluralOptions): Globalize.Pluralizer;
 
 		/**
 		 * @requires globalize/relative-time
 		 */
-		relativeTimeFormatter(unit: string, options?: TimeOptions): TimeFormatter;
+		relativeTimeFormatter(unit: string, options?: Globalize.TimeOptions): Globalize.TimeFormatter;
 	}
+
+	export = Globalize;
 }
