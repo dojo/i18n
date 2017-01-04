d7706a08bea34c906bf4d0731207f7d45f159f0b
Matt Wistrand <matt@troglodytidae.com>
2017-01-04 17:16:08 -0600

    Fix CLDR/globalize module IDs.
    - Update the module IDs used for Globalize.js and Cldr.js so that they
      can be used in both Node and AMD environments without modification.
    - Remove `global` from `cldr/load`.
    - Use `dojo-interfaces` where appropriate.

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,11 +4,11 @@ import has from 'dojo-core/has';
 import global from 'dojo-core/global';
 import { assign } from 'dojo-core/lang';
 import load from 'dojo-core/load';
-import { Handle } from 'dojo-core/interfaces';
+import { Handle } from 'dojo-interfaces/core';
 import Map from 'dojo-shim/Map';
 import Observable, { Observer, Subscription, SubscriptionObserver } from 'dojo-shim/Observable';
 import Promise from 'dojo-shim/Promise';
-import * as Globalize from 'globalize/globalize/message';
+import * as Globalize from 'globalize/dist/globalize/message';
 import loadCldrData from './cldr/load';
 import { generateLocales, normalizeLocale } from './util';
 
